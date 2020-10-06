3356ffc89c38d53360be6b07198e4724ff401ec1
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-05 23:15:29 -0400

    Create the RowMapper for Organization

---
+++ src/main/java/com/stradtkt/springdemo/daoimp/OrganizationRowMapper.java
@@ -0,0 +1,21 @@
+package com.stradtkt.springdemo.daoimp;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+import com.stradtkt.springdemo.domain.Organization;
+
+public class OrganizationRowMapper implements RowMapper<Organization> {
+
+	public Organization mapRow(ResultSet rs, int rownum) throws SQLException {
+		Organization org = new Organization();
+		org.setId(rs.getInt("id"));
+		org.setCompanyName(rs.getString("company_name"));
+		org.setYearsOfIncorporation(rs.getInt("years_of_incorporation"));
+		org.setPostalCode(rs.getString("postal_code"));
+		org.setEmployeeCount(rs.getInt("employee_count"));
+		org.setSlogan(rs.getString("slogan"));
+		return org;
+	}
+
+}
